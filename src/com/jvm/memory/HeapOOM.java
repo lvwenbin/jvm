c4d1c9f2371cb6971e3e5363292eaf4b6fb9f8b6
Administrator <Administrator@3OQMGBP8MXUUUXD>
2017-08-13 20:09:08 +0800

    内存溢出实例

--- src/com/jvm/memory/HeapOOM.java
+++ src/com/jvm/memory/HeapOOM.java
@@ -2,7 +2,11 @@ package com.jvm.memory;
 
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * VM Args：-Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
+ * 下述代码执行时有较大的风险,可能会导致操作系统假死。
+ * @author lvwenbin
+ */
 public class HeapOOM {
 	static class OOMObject {
     }
